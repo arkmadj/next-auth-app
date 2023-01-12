a74a41b25e1cc854b443f15ed328aa9f493f60f3
arkmadj <ahmadjinadu@gmail.com>
2023-01-12 23:53:58 +0100

    updated login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -7,11 +7,11 @@ const LoginPage = () => {
 
 	const onSubmit = async () => {};
 	return (
-		<div className="flex justify-center items-center h-screen bg-gradient-to-br from-cyan-300 to-sky-600">
-			<div className="px-7 py-4 bg-white rounded-md flex flex-col gap-2">
+		<div className="flex items-center justify-center h-screen bg-gradient-to-br from-cyan-300 to-sky-600">
+			<div className="flex flex-col gap-2 py-4 bg-white rounded-md px-7">
         <input type="text" onChange={e => userName.current = e.target.value}/>
         <input type="text" onChange={e => pass.current = e.target.value}/>
-        <input type="submit" value="Login" onClick={onSubmit}/>
+        <input className="w-full h-full bg-red-700" type="submit" value="Login" onClick={onSubmit}/>
       </div>
 		</div>
 	);

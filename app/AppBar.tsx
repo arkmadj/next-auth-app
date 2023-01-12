0215519ac7ad176b1a64cba7d756dbd641a11795
arkmadj <ahmadjinadu@gmail.com>
2023-01-12 08:29:07 +0100

    Added app bar component

---
+++ app/AppBar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+function AppBar() {
+	return (
+		<div className="bg-gradient-to-b from-cyan-50 to-cyan-200 p-2 flex gap-2">
+			<Link className="text-sky-600 hover:text-sky-700" href={"/"}>
+				Home
+			</Link>
+			<Link className="text-sky-600 hover:text-sky-700" href={"/admin"}>
+				Admin
+			</Link>
+			<Link className="text-sky-600 hover:text-sky-700" href={"/admin/panel"}>
+				Panel
+			</Link>
+		</div>
+	);
+}
+
+export default AppBar;

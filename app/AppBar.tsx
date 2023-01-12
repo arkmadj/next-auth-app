2c34c587eecaec41631a94f5695c8e27971e6558
arkmadj <ahmadjinadu@gmail.com>
2023-01-12 23:50:15 +0100

    Added signin/out template

--- app/AppBar.tsx
+++ app/AppBar.tsx
@@ -1,9 +1,11 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 function AppBar() {
+	const { data: session } = useSession();
 	return (
-		<div className="bg-gradient-to-b from-cyan-50 to-cyan-200 p-2 flex gap-2">
+		<div className="flex gap-2 p-2 bg-gradient-to-b from-cyan-50 to-cyan-200">
 			<Link className="text-sky-600 hover:text-sky-700" href={"/"}>
 				Home
 			</Link>
@@ -13,6 +15,20 @@ function AppBar() {
 			<Link className="text-sky-600 hover:text-sky-700" href={"/admin/panel"}>
 				Panel
 			</Link>
+			<div className="flex gap-2 ml-auto">
+				{session?.user ? (
+					<>
+						<p className="text-sky-600">{session.user.name}</p>
+						<button className="text-red-500" onClick={() => signOut()}>
+							Sign out
+						</button>
+					</>
+				) : (
+					<button className="text-green-600" onClick={() => signIn()}>
+						Sign in
+					</button>
+				)}
+			</div>
 		</div>
 	);
 }

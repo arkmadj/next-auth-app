f49c2cfde18ee3c52fb3615d9608f7cc9d596c42
arkmadj <ahmadjinadu@gmail.com>
2023-01-12 23:38:12 +0100

    Added strategy

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -16,26 +16,25 @@ export const authOptions: NextAuthOptions = {
 				password: { label: "Password", type: "password" },
 			},
 			async authorize(credentials, req) {
-				const { username, password } = credentials as any;
-				const res = await fetch("http://localhost:8080/auth/login", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						username,
-						password,
-					}),
-				});
+				// Add logic here to look up the user from the credentials supplied
+				const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
 
-				const user = await res.json();
-
-				if (res.ok && user) {
+				if (user) {
+					// Any object returned will be saved in `user` property of the JWT
 					return user;
-				} else return null;
+				} else {
+					// If you return null then an error will be displayed advising the user to check their details.
+					return null;
+
+					// You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+				}
 			},
 		}),
 	],
+
+	session: {
+		strategy: "jwt",
+	},
 };
 
 export default NextAuth(authOptions);

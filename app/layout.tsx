8a7d3e6cf47ce881d478c91fe9f42bd46af5b9d7
arkmadj <ahmadjinadu@gmail.com>
2023-01-12 23:45:17 +0100

    Added session

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,23 @@
 "use client";
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 import AppBar from "./AppBar";
 import "./globals.css";
 
 interface IProps {
 	children: ReactNode;
+	session: any;
 }
 
-export default function RootLayout({ children }: IProps) {
+export default function RootLayout({ children, session }: IProps) {
 	return (
 		<html lang="en">
 			<body>
-        <AppBar/>
-        <div className="h-screen">{children}</div>
-      </body>
+				<SessionProvider session={session}>
+					<AppBar />
+					<div className="h-screen">{children}</div>
+				</SessionProvider>
+			</body>
 		</html>
 	);
 }

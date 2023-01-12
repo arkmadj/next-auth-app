dbf45d104a3765217e5e70939c03336a51406991
arkmadj <ahmadjinadu@gmail.com>
2023-01-12 08:00:41 +0100

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+	return (
+		<h1 className="flex justify-center items-center p-5 text-green-500 text-lg font-bold">
+			This is the Home Page! Everyone can see it.
+		</h1>
+	);
 }
 
-export default HomePage
+export default HomePage;

4b3a3cde500c4a008188101fc83e0cadea4bd759
arkmadj <ahmadjinadu@gmail.com>
2023-01-12 08:32:42 +0100

    Added admin panel page

---
+++ app/admin/panel/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+	return (
+		<div className="flex justify-center items-center p-5 text-red-500 text-lg font-bold">
+			Deep protected page
+		</div>
+	);
+}
+
+export default page;

ef92710e07abef5ae100e1588d9abde4c9a1a13d
arkmadj <ahmadjinadu@gmail.com>
2023-01-12 23:56:51 +0100

    updated submit function

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,18 +1,34 @@
 "use client";
+import { signIn } from "next-auth/react";
 import React, { useRef } from "react";
 
 const LoginPage = () => {
 	const userName = useRef("");
 	const pass = useRef("");
 
-	const onSubmit = async () => {};
+	const onSubmit = async () => {
+		const result = await signIn("credentials", {
+			username: userName.current,
+			password: pass.current,
+			redirect: true,
+			callbackUrl: "/",
+		});
+	};
 	return (
 		<div className="flex items-center justify-center h-screen bg-gradient-to-br from-cyan-300 to-sky-600">
 			<div className="flex flex-col gap-2 py-4 bg-white rounded-md px-7">
-        <input type="text" onChange={e => userName.current = e.target.value}/>
-        <input type="text" onChange={e => pass.current = e.target.value}/>
-        <input className="w-full h-full bg-red-700" type="submit" value="Login" onClick={onSubmit}/>
-      </div>
+				<input
+					type="text"
+					onChange={(e) => (userName.current = e.target.value)}
+				/>
+				<input type="text" onChange={(e) => (pass.current = e.target.value)} />
+				<input
+					className="w-full h-full bg-red-700"
+					type="submit"
+					value="Login"
+					onClick={onSubmit}
+				/>
+			</div>
 		</div>
 	);
 };
